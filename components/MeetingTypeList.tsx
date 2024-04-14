9518294a12b6eeba0b84df5c7daf147bb0bdb6a5
Ravikant Chauhan <ravikantchauhan246@gmail.com>
2024-04-14 22:25:00 +0530

    Implemented Join Meeting

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -10,6 +10,7 @@ import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk'
 import { toast, useToast } from "@/components/ui/use-toast"
 import { Textarea } from './ui/textarea'
 import ReactDatePicker from 'react-datepicker';
+import { Input } from './ui/input'
 
 
 const MeetingTypeList = () => {
@@ -140,6 +141,20 @@ const MeetingTypeList = () => {
         buttonText="Start Meeting"
         handleClick={createMeeting}
       />
+      <MeetingModal
+        isOpen={meetingState === 'isJoiningMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Type or Paste the link here."
+        className="text-center"
+        buttonText="Join Meeting "
+        handleClick={()=>router.push(values.link)}
+      >
+        <Input placeholder='Meeting Link' className='border-none bg-dark-1 focus-visible:ring-0 focus-visible:ring-offset-2' 
+        onChange={(e)=> setValues({...values,link:e.target.value})}
+          
+        />
+        </MeetingModal>
+      
 
     </section>
   )
