380af952882ad78b063313a910eafe81778e0e1b
Ravikant Chauhan <ravikantchauhan246@gmail.com>
2024-04-15 21:34:37 +0530

    removed upcoming meeting timer

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -13,8 +13,7 @@ const date = now.toLocaleDateString('en-US', { month: 'long', day: 'numeric', ye
     
         <div className='h-[300px] w-full rounded-[20px] bg-hero bg-cover'>
           <div className='flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11'>
-          <h2 className="glassmorphism max-w-[400px] rounded py-2 text-center text-base font-normal ">Upcoming 
-          Meeting at 12:30 PM</h2>
+          
           <div className="flex flex-col gap-2">
             <h1 className='text-4xl font-extrabold lg:text-5xl'>
               {time}
